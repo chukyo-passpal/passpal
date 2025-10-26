54a19f655c99077d66953ebcb1a4847ccba61021
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-10-27 00:09:10 +0900

    イベント削除

--- src/domain/services/eventService.ts
+++ src/domain/services/eventService.ts
@@ -2,22 +2,12 @@ import remoteConfigProviderInstance from "@/src/data/providers/firebase/remoteCo
 
 export interface EventService {
     /**
-     * アプリ内のイベントを処理するオブジェクト
-     *
-     * 例:
-     * appEvent.addEventListener("eventName", callback);
-     */
-    readonly appEvent: EventTarget;
-
-    /**
      * アプリの初期化処理を実行します。
      */
     appInit: () => Promise<void>;
 }
 
 export class IntegratedEventService implements EventService {
-    public appEvent = new EventTarget();
-
     public async appInit(): Promise<void> {
         // 初期化処理があればここに追加
         await remoteConfigProviderInstance.fetchRemoteConfig();
