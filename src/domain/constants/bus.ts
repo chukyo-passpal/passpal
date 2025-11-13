bf18d46d2ce2699929f0bcb8d47ec30a71c5d60b
dokimiki <lmznd6yiew@gmail.com>
2025-11-14 00:48:50 +0900

    乗換のURL追加

--- src/domain/constants/bus.ts
+++ src/domain/constants/bus.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
-export const TRAIN_INFO_SERVICE = ["google-map", "yahoo-transit"] as const;
+export const TRAIN_INFO_SERVICE = ["google-map", "yahoo-transit", "jorudan", "navitime"] as const;
 export const TrainInfoServiceSchema = z.literal(TRAIN_INFO_SERVICE);
 export type TrainInfoService = (typeof TRAIN_INFO_SERVICE)[number];
 
 export const TrainInfoServiceName: Record<TrainInfoService, string> = {
     "google-map": "Google Map",
     "yahoo-transit": "Yahoo! 乗換案内",
+    jorudan: "ジョルダン乗換案内",
+    navitime: "NAVITIME",
 };
