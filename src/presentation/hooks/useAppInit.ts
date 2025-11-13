d0b527cf4acb92c70441287dcba4682d08c53e13
dokimiki <lmznd6yiew@gmail.com>
2025-11-14 01:23:42 +0900

    ユーザー情報処理を削除

--- src/presentation/hooks/useAppInit.ts
+++ src/presentation/hooks/useAppInit.ts
@@ -1,14 +1,11 @@
 import { useEffect } from "react";
-import { getAuth, getIdToken, GoogleAuthProvider, signInWithCredential } from "@react-native-firebase/auth";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 import { shibbolethWebViewRef } from "@/src/data/clients/chukyoShibboleth";
 import alboProviderInstance from "@/src/data/providers/chukyo-univ/alboProvider";
 import cubicsProviderInstance from "@/src/data/providers/chukyo-univ/cubicsProvider";
 import manaboProviderInstance from "@/src/data/providers/chukyo-univ/manaboProvider";
-import authRepositoryInstance from "@/src/data/repositories/authRepository";
-import useAuth from "./useAuth";
 import authServiceInstance from "@/src/domain/services/authService";
+import useAuth from "./useAuth";
 
 export default function useAppInit(shibRef: React.RefObject<shibbolethWebViewRef | null>) {
     /* フックでしか行えない初期化処理があればここに記述する */
@@ -23,28 +20,4 @@ export default function useAppInit(shibRef: React.RefObject<shibbolethWebViewRef
             authServiceInstance.setChukyoShibbolethAuthFunction(shibRef.current.auth);
         }
     }, [shibRef]);
-
-    /* 一時的にユーザー情報をサーバーへ送る処理 TODO: 消す */
-    useEffect(() => {
-        (async () => {
-            try {
-                // 1. GoogleにサインインしてIDトークン取得
-                await GoogleSignin.signInSilently();
-                const { idToken, accessToken } = await GoogleSignin.getTokens();
-                // 2. IDトークンからFirebase認証用Credentialを作成
-                const googleCredential = GoogleAuthProvider.credential(idToken, accessToken);
-                // 3. Firebaseにサインイン
-                const auth = getAuth();
-                await signInWithCredential(auth, googleCredential);
-                // 4. Firebase発行のIDトークンを取得
-                const currentUser = auth.currentUser;
-                if (!currentUser) return;
-                const firebaseIdToken = await getIdToken(currentUser);
-                if (!firebaseIdToken) return;
-                authRepositoryInstance.login(firebaseIdToken);
-            } catch (error) {
-                console.error("自動ログインに失敗しました: ", error);
-            }
-        })();
-    }, []);
 }
